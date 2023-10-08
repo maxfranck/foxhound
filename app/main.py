1bca220c699e2165b6c682fddb3ec5a7f86c6224
Max Franck <max.franck@gmail.com>
2023-10-08 05:38:12 -0300

    add two screens

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 # Load source image
 camera = cv2.VideoCapture(0)
@@ -8,15 +7,16 @@ while True:
     conectado, src = camera.read()
 
     # Remove noise by blurring with a Gaussian filter
-    src  = cv2.GaussianBlur(src,(3,3),0)
+    blur  = cv2.GaussianBlur(src,(3,3),0)
     # Convert the image to grayscale
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
     # Apply Laplace function
-    edges = cv2.Laplacian(src_gray, -1, ksize=5, scale=1,delta=0,borderType=cv2.BORDER_DEFAULT)
+    edges = cv2.Laplacian(gray, -1, ksize=5, scale=1,delta=0,borderType=cv2.BORDER_DEFAULT)
     # Converting back to uint8
-    resultado = cv2.convertScaleAbs(edges)
+    laplace = cv2.convertScaleAbs(edges)
     # Display the result
-    cv2.imshow('Video', resultado)
+    cv2.imshow('Drone' , src)
+    cv2.imshow('Laplace' , laplace)
 
     if cv2.waitKey(1) == 27: # Esc
         break
